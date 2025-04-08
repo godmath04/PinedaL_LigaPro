503ce502b234f859f49a6c2daaa366d4d0374bb0
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-04-08 08:35:28 -0500

    Se realizó el modelo, vista y controlador del Partido

---
+++ PinedaL_LigaPro/Models/Equipo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PinedaL_LigaPro.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre del equipo es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El nombre del equipo no puede exceder los 50 caracteres.")]
+        [Display(Name = "Nombre del Equipo")]
+        public string Nombre { get; set; }
+        [Range(0,20)]
+        public int PartidosJugados { get; set; }
+        [Range(0, 20)]
+        public int PartidosGanados { get; set; }
+        [Range(0, 20)]
+        public int PartidosPerdidos { get; set; }
+        [Range(0, 20)]
+        public int PartidosEmpatados { get; set; }
+
+        public int TotalPuntos { get; set; }
+
+    }
+}

5a65ec17212d933658a2c2f0ce9c7d20055c4923
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-04-13 13:39:32 -0500

    Actualizacion a la clase

--- PinedaL_LigaPro/Controllers/EquipoController.cs
+++ PinedaL_LigaPro/Controllers/EquipoController.cs
@@ -1,18 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using PinedaL_LigaPro.Models;
+using PinedaL_LigaPro.Repositories;
 
 namespace PinedaL_LigaPro.Controllers
 {
     public class EquipoController : Controller
     {
-        List<Equipo> equipos = new List<Equipo>();
-
-
-        public IActionResult List()
-        {
-            return View(equipos);
-        }
+        EquipoRepository repositorio = new EquipoRepository();
+        var equipos = repositorio.DevuelveListadoEquipos();
+        return View(equipos);
     }
 
 }
---
+++ PinedaL_LigaPro/Repositories/EquipoRepository.cs
@@ -0,0 +1,39 @@
+﻿using PinedaL_LigaPro.Models;
+namespace PinedaL_LigaPro.Repositories
+{
+    public class EquipoRepository
+    {
+        public IEnumerable<Equipo> DevuelveListadoEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo> ();
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "lDU",
+                PartidosJugados = 10,
+                PartidosGanados = 10,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 0,
+                TotalPuntos = 30
+            };
+
+            Equipo bsc = new Equipo
+            {
+                Id = 2,
+                Nombre = "BSC",
+                PartidosJugados = 10,
+                PartidosGanados = 1,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 9,
+                TotalPuntos = 3
+            };
+            equipos.Add (ldu);
+            equipos.Add(bsc);
+
+
+
+            return equipos;
+
+        }
+    }
+}

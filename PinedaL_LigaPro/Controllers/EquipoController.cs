66ee63ddbc47b3d81f2dc741fdac204fa4e16b42
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-04-13 13:08:08 -0500

    Esta es una prueba de cambios

--- PinedaL_LigaPro/Controllers/EquipoController.cs
+++ PinedaL_LigaPro/Controllers/EquipoController.cs
@@ -1,83 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PinedaL_LigaPro.Models;
 
 namespace PinedaL_LigaPro.Controllers
 {
     public class EquipoController : Controller
     {
-        // GET: EquipoController
-        public ActionResult Index()
-        {
-            return View();
-        }
+        List<Equipo> equipos = new List<Equipo>();
 
-        // GET: EquipoController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
-        // GET: EquipoController/Create
-        public ActionResult Create()
+        public IActionResult List()
         {
-            return View();
-        }
-
-        // POST: EquipoController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: EquipoController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: EquipoController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: EquipoController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: EquipoController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return View(equipos);
         }
     }
+
 }

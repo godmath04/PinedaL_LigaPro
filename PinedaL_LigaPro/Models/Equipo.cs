9e04567a732211050c25d5071e95f07cef0463fa
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-04-14 10:38:53 -0500

    FInalizado

--- PinedaL_LigaPro/Models/Equipo.cs
+++ PinedaL_LigaPro/Models/Equipo.cs
@@ -10,10 +10,10 @@ namespace PinedaL_LigaPro.Models
         [MaxLength(50, ErrorMessage = "El nombre del equipo no puede exceder los 50 caracteres.")]
         [Display(Name = "Nombre del Equipo")]
         public string Nombre { get; set; }
-        [Range(0,20)]
+        
         public string Descripcion { get; set; }
-        public string LogoUrl { get; set; }
-
+        public string? LogoUrl { get; set; }
+        [Range(0, 20)]
         public int PartidosJugados { get; set; }
         [Range(0, 20)]
         public int PartidosGanados { get; set; }
